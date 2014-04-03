a18272fbb402b427630750739638d4e9c94cd38b
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-04-03 11:47:36 +0800

    adding transitions & texts to states

--- assets/js/visualizer.js
+++ assets/js/visualizer.js
@@ -1,5 +1,7 @@
 $(document).ready( function(){
 
+	statesPool = [];
+	transitions = [];
 	SVG = $('svg');
 	gCtxMenu = $('#gContextMenu');
 	gCtxMenuLi = $('#gContextMenu li');
@@ -8,21 +10,14 @@ $(document).ready( function(){
 	x = 0;
 	y = 0;
 	count = 0;
-	selected = null;
+	pathCounter = 0;
+	activePath = null;
+	activeState = null;
 	openState = null;
 
 	$(document).on('contextmenu', SVG, function(e){
 		// alert('Context Menu event has fired!');
 		return false;
-	});
-
-	SVG.on('mousedown', 'g', function(e){
-		selected = $(this);
-		if (e.which == 3) {
-			showContextMenu(e);
-		} else {
-			hideContextMenu();
-		}
 	});
 
 	function showContextMenu(e) {
@@ -39,51 +34,49 @@ $(document).ready( function(){
 		return gCtxMenu.is(":hidden");
 	}
 
-	SVG.on('mouseup', 'g',  function(e){
-		if (selected != null && e.which != 3) {			
-			x = e.pageX - off_left;
-			y = e.pageY - off_top;
-
-			if (openState == null) {
-				openState = $(this);
-				openState.prepend(
-					$(document.createElementNS('http://www.w3.org/2000/svg', 'line')).attr(
-						{'x1': x, 'y1': y, 'stroke': 'red', 'stroke-width': 2})
-				);
-			} else {
-				openState.find('line').attr({'x2': x, 'y2': y});
-				openState = null;
-			}
-		}
+	gCtxMenuLi.click(function(e){
+		gCtxMenu.hide();
 	});
 
-	SVG.on('mousemove', SVG, function(e){
-		if (selected != null && e.which != 3 && contextMenuHidden()) {
+	SVG.on('mousemove', function(e){
+		if (activeState != null && e.which != 3 && contextMenuHidden()) {
 			x = e.pageX - off_left;
 			y = e.pageY - off_top;
-			selected.find('text').attr({
-				'x': x - selected.find('text').width()/2,
-				'y': y - selected.find('text').height()*2
+			activeState.find('text').attr({
+				'x': x - activeState.find('text').width()/2,
+				'y': y - activeState.find('text').height()*2
 			});
-			selected.find('circle').attr({'cx': x, 'cy': y});
+			activeState.find('circle').attr({'cx': x, 'cy': y});
+		}
+
+		if (openState != null  && e.which != 3) {
+			x = e.pageX - off_left;
+			y = e.pageY - off_top;
+			x1 = $('#'+activePath).find('line').attr('x1');
+			y1 = $('#'+activePath).find('line').attr('y1');
+			textX = Math.abs(x1 - x)*.5 + Math.min(x1, x);
+			textY = Math.abs(y1 - y)*.5 + Math.min(y1, y);
+				
+			$('#'+activePath).find('line').attr({'x2': x, 'y2': y});
+			$('#'+activePath).find('text').attr({'x': textX, 'y': textY});			
 		}
 	});
 
 	SVG.on('mouseup', function(e){
-		if (selected != null && e.which != 3) {
+		if (activeState != null && e.which != 3) {
 			x = e.pageX - off_left;
 			y = e.pageY - off_top;
-			selected.find('text').attr({
-				'x': x - selected.find('text').width()/2,
-				'y': y - selected.find('text').height() + 50
+			activeState.find('text').attr({
+				'x': x - activeState.find('text').width()/2,
+				'y': y - activeState.find('text').height() + 50
 			});
-			selected.find('circle').attr({'cx': x, 'cy': y});
+			activeState.find('circle').attr({'cx': x, 'cy': y});
+			activeState = null;
 		}
-		selected = null;
 	});
 
 	SVG.on('mousedown', function(e){		
-		if (selected == null) {
+		if (activeState == null) {
 			if (e.which != 3) {
 				hideContextMenu();
 			} else {
@@ -92,19 +85,28 @@ $(document).ready( function(){
 			x = e.pageX - off_left;
 			y = e.pageY - off_top;
 
-			SVG.append($(document.createElementNS('http://www.w3.org/2000/svg', 'g')).attr({'id': 'q'+count, 'x': x, 'y': y}));
+			$('#states').append($(document.createElementNS('http://www.w3.org/2000/svg', 'g')).attr({'id': 'q'+count, 'x': x, 'y': y}));
 
 			$('#q'+count).append(
 				$(document.createElementNS('http://www.w3.org/2000/svg', 'circle')).attr(
-					{'cx': x, 'cy': y, 'r': 30, 'stroke': 'black', 'stroke-width': 1, 'fill': 'white'})
+					{'cx': x, 'cy': y, 'r': 30, 'stroke': 'black', 'stroke-width': 1, 'fill': 'white'}
+				)
 			).append(
 				$(document.createElementNS('http://www.w3.org/2000/svg', 'text')).attr(
-					{'x': x, 'y': y, 'stroke': 'black', 'fill': 'black'}).html('Q'+count)
+					{'x': x, 'y': y, 'stroke': 'black', 'fill': 'black'}
+				).html('Q'+count)
 			);
 
 			textWidth = $('#q'+count+' text').width()/2;
 			textHeight = $('#q'+count+' text').height()/4;
 			$('#q'+count+' text').attr({'x': x - textWidth, 'y': y + textHeight});
+
+			if (activePath != null) {
+				$('#'+activePath).attr({'x2': x, 'y2': y});
+				activePath = null;
+			}
+
+			// statesPool.push(new State('q'+count, x, y));
 			count++;
 		} else {
 			if (e.which != 3) {
@@ -115,14 +117,73 @@ $(document).ready( function(){
 		}
 	});
 
-	SVG.on('mousemove', SVG, function(e){
-		if (openState != null  && e.which != 3) {
-			openState.find('line').attr({'x2': e.pageX - off_left, 'y2': e.pageY - off_top});
+	$('#states').on('mousedown', 'g', function(e){
+		activeState = $(this);
+		if (e.which == 3) {
+			showContextMenu(e);
+		} else {
+			hideContextMenu();
 		}
 	});
 
-	gCtxMenuLi.click(function(e){
-		gCtxMenu.hide();
+	$('#states').on('mouseup', 'g',  function(e){
+		if (activeState != null && e.which != 3) {
+			x = e.pageX - off_left;
+			y = e.pageY - off_top;
+			x1 = $('#'+activePath).find('line').attr('x1');
+			y1 = $('#'+activePath).find('line').attr('y1');
+			textX = Math.abs(x1 - x)*.5 + Math.min(x1, x);
+			textY = Math.abs(y1 - y)*.5 + Math.min(y1, y);
+
+			if (openState == null) {
+				openState = $(this);
+				activePath = 'path' + pathCounter;
+				$('#transitions').append($(document.createElementNS('http://www.w3.org/2000/svg', 'g')).attr({'id': activePath}));
+				$('#'+activePath).append(
+					$(document.createElementNS('http://www.w3.org/2000/svg', 'line')).attr(
+						{'x1': x, 'y1': y, 'x2': x, 'y2': y, 'stroke': 'red', 'stroke-width': 2}
+					)
+				).append(
+					$(document.createElementNS('http://www.w3.org/2000/svg', 'text')).attr(
+						{'x': textX, 'y': textY, 'stroke': 'blue'}
+					).html('[a, b, ~]')
+				);
+			} else {
+				closeState = $(this);
+				$('#'+activePath).find('line').attr({'x2': x, 'y2': y});
+				$('#'+activePath).find('text').attr({'x': textX, 'y': textY});
+				pathCounter++;
+				activePath = null;
+				openState = null;
+				closeState = null;
+			}
+			console.log(openState + ' : ' + activeState + ' : ' + activePath);
+		}
 	});
 
 });
+
+
+
+
+
+
+
+
+
+function State(label, x, y) {
+	this.label = label;
+	this.x = x;
+	this.y = y;
+	this.transitions = {};
+	this.final = false;
+}
+
+function Transition(state, x1, y1) {
+	this.openState = state;
+	this.closeState = null;
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = null;
+	this.y2 = null;
+}
